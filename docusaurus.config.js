2cbbba861425ef14706a458e56bd59e53df8880a
alex <apopov@xbsoftware.by>
2021-10-19 16:18:24 +0300

    [upd] Examples link updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -188,7 +188,7 @@ module.exports = {
             },
             items: [{
                     label: 'Examples',
-                    href: 'https://snippet.dhtmlx.com/atl9gd4h?text=#spreadsheet',
+                    href: 'https://snippet.dhtmlx.com/ihtkdcoc?text=%23spreadsheet',
                     position: 'right',
                 },
                 {
